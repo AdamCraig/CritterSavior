c11c4a5c20fc350c6165b17533117ccb0675f12e
Adam Craig <lateralusajc@gmail.com>
2016-07-08 16:30:39 -0700

    Create PetPagerAdapter.java to connect Pet data to the PetProfileFragment

---
+++ app/src/main/java/com/epicodus/supremeinvention/adapters/PetPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.epicodus.supremeinvention.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.epicodus.supremeinvention.models.Pet;
+import com.epicodus.supremeinvention.ui.PetProfileFragment;
+
+import java.util.ArrayList;
+
+public class PetPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Pet> mPets;
+
+    public PetPagerAdapter(FragmentManager fm, ArrayList<Pet> pets) {
+        super(fm);
+        mPets = pets;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PetProfileFragment.newInstance(mPets.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mPets.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mPets.get(position).getName();
+    }
+}

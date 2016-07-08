5b66d3c30ce28123afec948a6a5f1ec1d21c16a6
Adam Craig <lateralusajc@gmail.com>
2016-07-08 08:49:12 -0700

    Add Constants.java and contents of gradle.properties

---
+++ app/src/main/java/com/epicodus/supremeinvention/Constants.java
@@ -0,0 +1,6 @@
+package com.epicodus.supremeinvention;
+
+public class Constants {
+    public static final String PET_CONSUMER_KEY = BuildConfig.PET_CONSUMER_KEY;
+    public static final String PET_BASE_URL = "https://api.petfinder.com/pet.find?key=";
+}

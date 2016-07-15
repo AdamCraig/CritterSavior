e34886582c941427f257c705093cc29423c27e2d
Adam Craig <lateralusajc@gmail.com>
2016-07-15 08:52:52 -0700

    Create new activity CreateAccountActivity with corresponding XML layout

---
+++ app/src/main/java/com/epicodus/supremeinvention/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.supremeinvention.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.supremeinvention.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
